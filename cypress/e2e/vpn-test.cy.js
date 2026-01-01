0e5047cacaf99de80c1743c03fae51fe770e7aaa
Łukasz Posmyk <lukasz@MacBook-Air-ukasz.local>
2026-01-01 21:03:59 +0100

    chore/circleci-config

--- cypress/e2e/vpn-test.cy.js
+++ cypress/e2e/vpn-test.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://admin-3.phq-preprod.click/')
-  })
-})
+describe('Weryfikacja połączenia VPN', () => {
+  it('powinien pokazać adres IP serwera VPN', () => {
+    // Odwiedzamy stronę, która zwraca tylko czysty tekst z adresem IP
+    cy.visit('https://ifconfig.me/ip');
+
+    // Pobieramy treść strony (czyli IP) i wypisujemy ją w logach
+    cy.get('body').then(($body) => {
+      const currentIp = $body.text().trim();
+      cy.log('Aktualny adres IP to: ' + currentIp);
+      
+      expect(currentIp).to.eq('219.100.37.64');
+    });
+  });
+});
