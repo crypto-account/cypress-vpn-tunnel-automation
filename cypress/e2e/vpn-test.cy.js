6b8fb4b66a83173b09b9be9f27e25a43c3fc42c8
Łukasz Posmyk <lukasz@ip-192-168-30-11.eu-west-1.compute.internal>
2026-01-03 17:36:09 +0100

    add host name

--- cypress/e2e/vpn-test.cy.js
+++ cypress/e2e/vpn-test.cy.js
@@ -1,7 +1,7 @@
 describe('Weryfikacja połączenia VPN', () => {
   it('powinien pokazać adres IP serwera VPN', () => {
     // Odwiedzamy stronę, która zwraca tylko czysty tekst z adresem IP
-    cy.visit('https://ifconfig.me/ip');
+    cy.visit('https://public-vpn-131.opengw.net/');
 
     // Pobieramy treść strony (czyli IP) i wypisujemy ją w logach
     cy.get('body').then(($body) => {
